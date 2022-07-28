4a0c64cc81d8c56be179afa93af3f3bb6c975f9c
Ardhan12 <arief.ramadhan43@gmail.com>
2022-07-28 10:54:13 +0700

    Initial Commit

---
+++ ModelDataPractice/ModelDataPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelDataPracticeApp.swift
+//  ModelDataPractice
+//
+//  Created by Arief Ramadhan on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ModelDataPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
